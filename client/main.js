ae1661fde485ae67630b0eb1618120d0da7c6127
Rowan Merewood <rowan@merewood.org>
2020-05-17 23:38:52 +0100

    Size canvas to container

--- client/main.js
+++ client/main.js
@@ -16,22 +16,46 @@ import 'input-knob';
 
 class RotavoApp {
   constructor(els) {
+    // just for type-hinting
+    this.els = {
+      casing: null,
+      screen: null,
+      activeSketch: null,
+      horzKnob: null,
+      vertKnob: null,
+    };
+
     this.x = 10;
     this.y = 10;
+    this.xMax = 100;
+    this.yMax = 100;
+    this.xRatio = 1;
+    this.yRatio = 1;
     this.updating = false;
 
     this.els = els;
+
+    this.sizeCanvas = this.sizeCanvas.bind(this);
+    this.handleKnob = this.handleKnob.bind(this);
+    this.updateSketch = this.updateSketch.bind(this);
+
+    this.sizeCanvas();
+    this.els.casing.addEventListener('knob-move-change', this.handleKnob);
+  }
+
+  sizeCanvas() {
+    this.els.activeSketch.width = this.els.screen.offsetWidth;
+    this.els.activeSketch.height = this.els.screen.offsetHeight;
     this.ctx = this.els.activeSketch.getContext('2d');
-    this.ctx.moveTo(this.x, this.y);
+
+    this.xRatio = this.els.activeSketch.width / this.xMax;
+    this.yRatio = this.els.activeSketch.height / this.yMax;
+
+    this.ctx.moveTo(this.x * this.xRatio, this.y * this.yRatio);
     this.ctx.strokeStyle = '#AA471F'
     this.ctx.lineWidth = 2;
     this.ctx.lineJoin = 'round';
     this.ctx.lineCap = 'round';
-
-    this.handleKnob = this.handleKnob.bind(this);
-    this.updateSketch = this.updateSketch.bind(this);
-
-    this.els.casing.addEventListener('knob-move-change', this.handleKnob);
   }
 
   handleKnob(e) {
@@ -42,7 +66,7 @@ class RotavoApp {
     if (!this.updating) {
       this.x = this.els.horzKnob.value;
       this.y = this.els.vertKnob.value;
-      this.ctx.lineTo(this.x, this.y);
+      this.ctx.lineTo(this.x * this.xRatio, this.y * this.yRatio);
       this.ctx.stroke();
     }
   }
@@ -53,6 +77,7 @@ window.customElements.whenDefined('input-knob').then(initApp());
 function initApp() {
   const app = new RotavoApp({
     casing: document.querySelector('.casing'),
+    screen: document.querySelector('.screen'),
     activeSketch: document.querySelector('.active-sketch'),
     horzKnob: document.querySelector('.horz-knob'),
     vertKnob: document.querySelector('.vert-knob'),
